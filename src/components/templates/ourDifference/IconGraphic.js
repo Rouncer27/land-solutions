4d582d0f7739b33ade585913a0efda04a8305794
Rouncer27 <trevor@switchbackcreative.ca>
2022-07-15 11:36:45 -0600

    added suzy animations

--- src/components/templates/ourDifference/IconGraphic.js
+++ src/components/templates/ourDifference/IconGraphic.js
@@ -1,13 +1,53 @@
-import React from "react"
+import React, { useEffect } from "react"
 import styled from "styled-components"
 
 import icon from "../../../images/icon.png"
-import waves from "../../../images/waves.png"
+import waves from "../../../images/long-wave.png"
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+gsap.registerPlugin(ScrollTrigger)
 
 const IconGraphic = () => {
+  useEffect(() => {
+    const tigger = document.querySelector("#icon-graphic-trigger")
+    const waves = document.querySelector(".wave-bg")
+    const icon = document.querySelector(".icon-graphic")
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: tigger,
+          markers: false,
+          start: "top 65%",
+          toggleActions: "play none none reverse",
+          scrub: false,
+        },
+      })
+      .to(icon, {
+        rotation: 360,
+        ease: "power3.out",
+        duration: 2,
+      })
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: tigger,
+          markers: false,
+          start: "top 87.5%",
+          end: "bottom 30%",
+          toggleActions: "play none none reverse",
+          scrub: 5,
+        },
+      })
+      .to(waves, {
+        x: 200,
+        ease: "none",
+      })
+  }, [])
   return (
-    <StyledDiv>
-      <div className="icon">
+    <StyledDiv id="icon-graphic-trigger">
+      <div className="icon-graphic">
         <img src={icon} alt="" />
       </div>
       <div className="wave-bg" />
@@ -18,7 +58,7 @@ const IconGraphic = () => {
 const StyledDiv = styled.div`
   position: relative;
 
-  .icon {
+  .icon-graphic {
     position: relative;
     width: 100%;
     max-width: 31.5rem;
@@ -28,14 +68,15 @@ const StyledDiv = styled.div`
 
   .wave-bg {
     position: absolute;
-    top: 10%;
-    bottom: 10%;
-    left: 0;
-    width: 100%;
+    top: -10rem;
+    right: -45%;
+    width: 200%;
+    height: 30rem;
     background-image: url(${waves});
     background-size: 100% 100%;
     background-repeat: no-repeat;
     background-position: center;
+    z-index: 1;
   }
 `
 
