29bdd261adbcd909d5d0a9bb73fb295900a0dcfb
Rouncer27 <trevor@switchbackcreative.ca>
2022-09-26 11:25:23 -0600

    fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,9 @@ module.exports = {
       options: {
         trackingIds: ["G-3VBE7MLB82"],
       },
+      pluginConfig: {
+        head: true,
+      },
     },
     {
       resolve: "gatsby-plugin-robots-txt",
